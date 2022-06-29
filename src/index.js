6e1e895de04083a0953b4657922eeafd01e66762
timohaars <timoha.ars@gmail.com>
2022-06-29 08:49:52 +0300

    feat: add counter up to 99

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable (value) {
+    let str = String(value);
+    let index = str.length - 1;
+    let result;
+    let ones = Array('','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine');
+    let teens = Array('ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen');
+    let tens = Array('', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety');
+    if (str === '0') {
+     return 'zero';
+    }
+     if (str.length === 1) {
+         result = ones[Number(str)];
+     } else if ((str.length === 2) && (value<20)) {
+        result = teens[Number(str[str.length-1])];
+     } else if ((str.length === 2) && (value >= 20) && (value%10 === 0)){
+        result = tens[Number(str[0])];
+     } else if ((str.length === 2) && (value >= 20) && (value%10 !== 0)) {
+        result = `${tens[Number(str[0])]} ${ones[Number(str[str.length-1])]}`;
+     }
+
+     return result;
+ } 
